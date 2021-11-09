2a2967e210aae1c6618b6833ac8f3162fb925910
AnonymousV1Digi <93959887+AnonymousV1Digi@users.noreply.github.com>
2021-11-09 10:39:19 +0800

    Create datad.sh

---
+++ datad.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+wget -O welcome "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/welcome.sh"
+wget -O number "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/number.sh"
+wget -O menu "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/menu.sh"
+wget -O login "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/login.sh"
+wget -O tac "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/tac.sh"
+
+#getlink
+wget -O rm1 "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/rm1.sh"
+wget -O validity "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/validity.sh"
+wget -O free3gb "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/free3gb.sh"
+wget -O free6gb "https://raw.githubusercontent.com/AnonymousV1Digi/data/main/free6gb.sh"
+
+#permission
+chmod +x number.sh
+chmod +x menu.sh
+chmod +x rm1.sh
+chmod +x validity.sh
+chmod +x free3gb.sh
+chmod +x free6gb.sh
+clear
