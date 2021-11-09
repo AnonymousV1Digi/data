b81852280571c2cefd4405897bca0a5a05b2e94f
AnonymousV1Digi <93959887+AnonymousV1Digi@users.noreply.github.com>
2021-11-09 11:13:40 +0800

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -4,16 +4,16 @@ echo -e ""
 echo -e "Selamat Datang ke Sistem Digi"
 echo -e ""
 
-number=$(cat /etc/digi/number)
+number=$(cat /etc/number)
 echo -e "Sila masukkan nombor telefon Digi anda dimulai dengan nombor 6. Contoh 60134567891"
 read -p "Nombor telefon Digi: " number
-rm -f /etc/digi/number
+rm -f /etc/number
 clear
-mkdir /etc/digi/number;
+mkdir /etc/number;
 clear
 echo -e "Sila masukkan nombor sekali lagi"
 read -p "Nombor telefon Digi: " number
-echo "$number" >> /etc/digi/number
+echo "$number" >> /etc/number
 
 echo -e ""
 echo -e "Terima kasih"
